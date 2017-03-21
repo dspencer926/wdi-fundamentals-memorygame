60b3e506886f04ed462f64d5a380080137e712cf
David Spencer <davidspencer@Davids-MacBook-Pro-2.local>
2017-03-21 00:28:58 -0400

    added if loops'
    /

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = 'king';
 var cardTwo = 'king'
 var cardThree = 'queen';
 var cardFour = 'queen';
+
+if (cardOne === cardTwo) {
+	alert ('Greatjob');
+};
+if (cardOne != cardTwo) {
+	alert ('you done fucked up');
+};
