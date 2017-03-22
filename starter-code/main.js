98cb917eaea018537f065b33954719012c32727b
David Spencer <davidspencer@Davids-MacBook-Pro-2.local>
2017-03-22 12:48:34 -0400

    added cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,20 @@ var cardTwo = 'king'
 var cardThree = 'queen';
 var cardFour = 'queen';
 
-if (cardOne === cardTwo) {
+var gameboard = document.getElementById('game-board');
+
+var createCards = function(){
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.setAttribute('class', 'card');
+		document.getElementById('game-board').appendChild(newCard);
+	};
+};
+
+/*if (cardOne === cardTwo) {
 	alert ('Greatjob');
 };
 if (cardOne != cardTwo) {
 	alert ('you done fucked up');
 };
+*/
